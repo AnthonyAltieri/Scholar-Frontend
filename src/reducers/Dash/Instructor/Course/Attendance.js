e2eb41db41ace2fd94724ae42f35c94816953686
anthonyaltieri <Anthonyraltieri@gmail.com>
2017-01-02 20:58:47 -0800

    reset ask stats on instructor side when course session ends

--- src/reducers/Dash/Instructor/Course/Attendance.js
+++ src/reducers/Dash/Instructor/Course/Attendance.js
@@ -11,6 +11,10 @@ const initialState = {
 const Attendance = (state = initialState, action) => {
   switch (action.type) {
 
+    case 'ENDED_COURSESESSION': {
+      return initialState;
+    }
+
     // When course activated reset attendance stats
     case 'ACTIVATE_COURSE': {
       console.log("Fire em up: ACTIVATE COURSE");
